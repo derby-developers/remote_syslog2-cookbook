4b08d79c83706c995b756d19a1e75dbedd4795e1
Eric Gay <eric.gay@gmail.com>
2016-05-05 15:35:23 -0400

    force the mv of the bin

--- recipes/install.rb
+++ recipes/install.rb
@@ -11,7 +11,7 @@ bash 'extract remote_syslog2' do
   code <<-EOH
     mkdir -p #{install['extracted_path']}
     tar xzf #{install['download_path']} -C #{install['extract_path']}
-    mv #{install['extracted_path']}/#{install['extracted_bin']} #{bin_file}
+    mv -f #{install['extracted_path']}/#{install['extracted_bin']} #{bin_file}
     rm -rf #{install['download_path']} #{install['extracted_path']}
   EOH
   not_if { ::File.exists?(bin_file) }
